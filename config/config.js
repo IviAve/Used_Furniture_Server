2f5d51ece36cfe62fceeced81f408b340cc6e508
IvaDeneva <denevaiva79@gmail.com>
2025-04-08 13:59:07 +0300

    config

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
     development: {
-        port: process.env.PORT || 3000,
+        port: process.env.PORTS || 3000,
         // dbURL: 'mongodb://localhost:27017/Used-Furniture',
         dbURL: process.env.CLOUD_DB_URL,
         origin: ['http://localhost:5555', 'http://localhost:4200']
     },
     production: {
-        port: process.env.PORT || 3000,
+        port: process.env.PORTS || 3000,
         dbURL: process.env.DB_URL_CREDENTIALS,
         origin: []
     }
