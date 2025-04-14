2f714337fd47133c0d73828b3ccb4695e2ed6541
IvaDeneva <denevaiva79@gmail.com>
2025-04-14 15:11:56 +0300

    test config

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
     development: {
-        port: process.env.PORT || PORT,
-        // dbURL: 'mongodb://localhost:27017/Used-Furniture',
-        dbURL: process.env.DB_URL_CREDENTIALS,
-    origin: ['mongodb://localhost:27017/Used-Furniture']
+        port: process.env.PORT || 3000,
+        dbURL: 'mongodb://localhost:27017/Used-Furniture',
+        // dbURL: process.env.DB_URL_CREDENTIALS,
+    origin: ['http://localhost:3000']
     },
     production: {
-        port: process.env.PORT || 3000,
+        port: process.env.PORT || 8080,
         dbURL: process.env.DB_URL_CREDENTIALS,
         origin: ['https://used-furniture-680be.web.app']
     }
